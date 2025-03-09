e6d01e3571bfc0954ac2cd3e43342a33b61b20a3
SantiagoMunoz <smr.rambar@gmail.com>
2025-03-09 21:44:27 +0000

    Added MOTD quotes

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -10,12 +10,15 @@
 (setq-default initial-scratch-message
               (seq-random-elt '(
                                 ";; There is nothing either good or bad,\n;; but thinking makes it so.\n;; - William Shakespeare\n\n"
-                                ";; Even  in Kyoto\n;; hearing the cuckoo's cry\n;; I long for Kyoto.\n;; -Bashō\n\n"
+                                ";; Even  in Kyoto\n;; hearing the cuckoo's cry\n;; I long for Kyoto.\n;; - Bashō\n\n"
                                 ";; When you're dead and you look back at your life,\n;; you'll see that none of this mattered in the least.\n;; - Kōdō Sawaki\n\n"
                                 ";; A day without work is a day without food.\n;; - Baizhang Huaihai\n\n"
                                 ";; Nothing accelerates rot faster than neglect.\n;; - Andrew Hunt\n\n"
                                 ";; You can change your organization,\n;; or change your organization.\n;; - Martin Fowler\n\n"
-                                ";; I'm not in this world to live up to your expectations\n;; and you're not in this world to live up to mine.\n;; - Bruce Lee\n\n")))
+                                ";; I'm not in this world to live up to your expectations\n;; and you're not in this world to live up to mine.\n;; - Bruce Lee\n\n"
+                                ";; Every day is a journey, and  thejourney itself is home.\n;; - Matsuo Basho\n\n"
+                                ";; I expect to pass through this world but once; if, therefore, there be any kindness I can show,\n;; or any good thing I can do my fellow being, let me do it now,\n;; let me not defer, nor neglect it, for I shall not pass this way again.\n;; - Broadside\n\n"
+                                )))
 
 ;; I like to see a bit of the wallpaper as a backdrop while I work
 (sanityinc/adjust-opacity nil -10)
@@ -40,7 +43,7 @@
 (setq org-roam-directory "~/roam")
 (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
 (global-set-key (kbd "C-c n f") 'org-roam-node-find)
-(global-set-key (kbd "C-c n l") 'org-roam-node-toggle)
+set-key (kbd "C-c n l") 'org-roam-node-toggle
 (org-roam-db-autosync-mode)
 
 (provide 'init-local)
